8d7673bcf4b1d543001b017a84643d413ba2d438
Mariia Voitenko <mariya.voytenko@gmail.com>
2016-05-15 23:15:11 +0300

    Some changed

--- src/main/java/com/voitenko/diploma/web/rest/SightseeingResource.java
+++ src/main/java/com/voitenko/diploma/web/rest/SightseeingResource.java
@@ -5,8 +5,10 @@ import com.voitenko.diploma.domain.Sightseeing;
 import com.voitenko.diploma.repository.SightseeingRepository;
 import com.voitenko.diploma.web.rest.util.HeaderUtil;
 import com.voitenko.diploma.web.rest.util.PaginationUtil;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
@@ -17,10 +19,15 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 /**
  * REST controller for managing Sightseeing.
@@ -30,10 +37,10 @@ import java.util.Optional;
 public class SightseeingResource {
 
     private final Logger log = LoggerFactory.getLogger(SightseeingResource.class);
-        
+
     @Inject
     private SightseeingRepository sightseeingRepository;
-    
+
     /**
      * POST  /sightseeings : Create a new sightseeing.
      *
@@ -94,9 +101,34 @@ public class SightseeingResource {
     public ResponseEntity<List<Sightseeing>> getAllSightseeings(Pageable pageable)
         throws URISyntaxException {
         log.debug("REST request to get a page of Sightseeings");
-        Page<Sightseeing> page = sightseeingRepository.findAll(pageable); 
+        Page<Sightseeing> page = sightseeingRepository.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/sightseeings");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "sightseeings/image/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> downloadUserAvatarImage(@PathVariable Long id) throws IOException {
+        Properties properties = new Properties();
+        String folder = "";
+        String resourceName = "content.properties";
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        try (InputStream inputStream = loader.getResourceAsStream(resourceName)) {
+            properties.load(inputStream);
+            folder = properties.getProperty("folder");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
+        Sightseeing sightseeing = sightseeingRepository.findOne(id);
+        InputStream in = new BufferedInputStream(new FileInputStream(folder  + sightseeing.getPhoto()));
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
     }
 
     /**
